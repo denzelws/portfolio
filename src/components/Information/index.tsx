9933d9facb31f4c23a20fc9ce5fae45e52f834ed
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-16 14:21:47 -0300

    Refactor Information styles

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -2,33 +2,36 @@ import React from 'react';
 import { BsArrowDownShort } from 'react-icons/bs';
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 
-const Index = () => {
-  return (
-    <div className='h-screen flex justify-center'>
-      <div className='h-3/6 flex flex-col justify-center items-center relative'>
-        <div>
-          <h1 className='text-5xl lg:text-7xl text-center'>
-            FRONT END DEV.
-            <br />
-            WEB DESIGN
-          </h1>
-        </div>
 
-        <span className='text-5xl animate-bounce mt-4 cursor-pointer'>
-          <BsArrowDownShort style={{ fill: 'purple' }} />
+import Scroll from '../../components/Scroll'
+
+const Information = () => {
+ 
+  return (
+    <div className='h-[85vh]'>
+      <div className='h-full flex flex-col justify-center items-center'>
+       
+        <div className='flex flex-col items-center gap-10 h-max'>
+        <h1 className='text-5xl text-center lg:text-7xl'>
+          FRONT END DEV.
+          <br/>
+          WEB DESIGNER
+        </h1>
+
+        <span className='text-6xl animate-bounce cursor-pointer'>
+          <BsArrowDownShort style={{ fill: 'purple' }}/>
         </span>
 
-        <div className='mt-8 flex items-center space-x-4 text-3xl lg:text-4xl py-3 text-gray-600'>
-          <a href='https://www.linkedin.com/in/dzwashington/' target='_blank' rel='noopener noreferrer'>
-            <AiFillLinkedin  />
-          </a>
-          <a href="https://github.com/denzelws" target='_blank' rel='noopener noreferrer'>
-            <AiFillGithub  />
-          </a>
+        <div className='flex text-3xl lg:text-4xl text-gray-500 gap-5 mt-10 cursor-pointer'>
+          <AiFillLinkedin/>
+          <AiFillGithub/>
         </div>
+        </div>
+
       </div>
+        <Scroll />
     </div>
   );
 };
 
-export default Index;
+export default Information;
