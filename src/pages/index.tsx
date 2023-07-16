404d74c5f8c9e659ad2d20da8b7f51aa29cf290e
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-16 17:44:29 -0300

    Add Scroll and Details to Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Details from '@/components/Details';
 import Header from '../components/Header'
 import Information from '../components/Information'
 import Scroll from '../components/Scroll'
@@ -7,6 +8,8 @@ export default function Home() {
     <div className='px-10 min-h-screen bg-gradient-to-br from-gray-100 to-gray-50'>
       <Header />
       <Information />
+      <Scroll />
+      <Details />
     </div>
   );
 }
