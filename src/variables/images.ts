4a3a5c0a498e0dd5e4ca0e9a2b7d5d80c1b61208
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-07-16 14:21:17 -0300

    Add variables folder to handle imgs

---
+++ src/variables/images.ts
@@ -0,0 +1,15 @@
+import figma from '../img/figma.png'
+import next from '../img/next.png'
+import react from '../img/react.png'
+import js from '../img/js.png'
+import ts from '../img/ts.png'
+import node from '../img/node.png'
+
+export const images = [
+  figma,
+  next,
+  react,
+  js,
+  ts,
+  node
+  ]
